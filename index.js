29a6996a4fe4545d3fa513c8d0a70b18d142fae3
Archit <archit_kumar13@outlook.com>
2024-04-17 15:49:52 +0530

    Committed node files...

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.json({
+        message:"You are visiting root..."
+    })
+})
+app.get("/login", (req, res) => {
+    res.json({
+        message:"You are visiting login_route..."
+    })
+})
+app.get("/signup", (req, res) => {
+    res.json({
+        message:"You are visiting signup_route..."
+    })
+})
+
+
+app.listen(8000, () => {
+console.log("App is running on 8000");
+});
